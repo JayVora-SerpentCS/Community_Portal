d180035936741798302efe69594f1517eea7f238
Chirag <chirag.patel.serpentcs@gmail.com>
2019-08-09 15:56:42 +0530

    [MIG][12.0] Migrate module.

--- cp_businessdiary/__manifest__.py
+++ cp_businessdiary/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
 {
     'name': 'Business Diary For Community Portal',
     'category': 'Website',
     'summary': 'Business Diary Management',
-    'version': '10.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'website': 'http://www.serpentcs.com',
     'author': 'Serpent Consulting Services Pvt. Ltd.',
@@ -16,7 +15,7 @@
     'data': [
         'security/portal_security.xml',
         'security/ir.model.access.csv',
-        'views/menu_details.xml',
+        'data/website_menu.xml',
         'views/templates.xml',
         'views/template_input_business.xml',
         'views/view_business_diary.xml',
